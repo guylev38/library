0836360fa9d123c23c445cc01f6292d4839a02c2
Guy <guydovlev@gmail.com>
2023-08-05 14:46:41 +0300

    Added pop-up form

---
+++ main.js
@@ -0,0 +1,13 @@
+let books = [];
+
+
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('#add-btn');
+const closeModal = document.querySelector('.submit-btn');
+
+openModal.addEventListener('click', () => {
+  modal.showModal();
+});
+
+
+
